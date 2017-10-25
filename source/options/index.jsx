71a03a5d8a3221b17284efcc33fd53ed426949be
tryasko <user17@user17s-MacBook-Pro.local>
2017-10-25 18:33:28 +0300

    Test jsx

--- source/options/index.jsx
+++ source/options/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Router from './router';
+import Router from './router/index.jsx';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
