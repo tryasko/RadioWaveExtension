8cf04e7c70c97246bf967610ccdd18c84230476f
tryasko <tryasko@gmail.com>
2017-11-02 17:33:01 +0200

    move old files to hidden folder

--- source/old-files/options/settings/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-import './index.css';
-
-export default class Settings extends Component {
-  render() {
-    return <h1 className="Settings">The settings page in progress...</h1>;
-  }
-}
