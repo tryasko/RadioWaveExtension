a689041ef0ffeead951a5a39d8eaeb7d0cb23608
tryasko <tryasko@gmail.com>
2018-02-19 16:15:29 +0200

    Update station list (Delete dead stations)

--- popup/stations.js
+++ popup/stations.js
@@ -1,156 +1,141 @@
 window.stationList = [
   {
-    name: "KissFM UA",
+    name: "Ua KissFM",
     stream: "http://online-kissfm.tavrmedia.ua/KissFM"
   },
 
   {
-    name: "Radio ROKS",
+    name: "Ua Radio ROKS",
     stream: "http://online-radioroks.tavrmedia.ua/RadioROKS"
   },
 
   {
-    name: "Radio Feel",
+    name: "Ua Radio Feel",
     stream: "http://62.80.190.246:8000/Feel"
   },
 
   {
-    name: "Pirate Station",
+    name: "Rcd Pirate Station",
     stream: "http://air2.radiorecord.ru:805/ps_128"
   },
 
   {
-    name: "Radio Record",
+    name: "Rcd Radio Record",
     stream: "http://air2.radiorecord.ru:805/rr_128"
   },
 
   {
-    name: "NRJ Ru",
+    name: "Ru NRJ",
     stream: "http://ic4.101.ru:8000/v1_1"
   },
 
   {
-    name: "Maximum",
-    stream: "http://icecast.radiomaximum.cdnvideo.ru/maximum.mp3"
-  },
-
-  {
-    name: "GarageFM",
+    name: "Pdj GarageFM",
     stream: "http://91.213.196.100:8000/garagefm-192"
   },
 
   {
-    name: "TOP 100",
+    name: "Pdj TOP 100",
     stream: "http://91.213.196.100:8000/top100-192"
   },
 
   {
-    name: "Old-School",
+    name: "Pdj Old-School",
     stream: "http://91.213.196.100:8000/oldschool-192"
   },
 
   {
-    name: "Channel 5",
+    name: "Pdj Channel 5",
     stream: "http://91.213.196.100:8000/channel5-192"
   },
 
   {
-    name: "Too Deep",
-    stream: "http://91.213.196.100:8000/toodeep-192"
-  },
-
-  {
-    name: "Klubb",
+    name: "Pdj Klubb",
     stream: "http://91.213.196.100:8000/klubb-192"
   },
 
   {
-    name: "Dubstep",
+    name: "Pdj Dubstep",
     stream: "http://91.213.196.100:8000/dubstep-192"
   },
 
   {
-    name: "Nu",
+    name: "Pdj Nu",
     stream: "http://91.213.196.100:8000/nu-192"
   },
 
   {
-    name: "Too Nu",
+    name: "Pdj Too Nu",
     stream: "http://91.213.196.100:8000/toonu-192"
   },
 
   {
-    name: "Yo",
+    name: "Pdj Yo",
     stream: "http://91.213.196.100:8000/yo-192"
   },
 
   {
-    name: "Full Moon",
+    name: "Pdj Full Moon",
     stream: "http://91.213.196.100:8000/fullmoon-192"
   },
 
   {
-    name: "Vata",
+    name: "Pdj Vata",
     stream: "http://91.213.196.100:8000/vata-192"
   },
 
   {
-    name: "300kmh",
+    name: "Pdj 300kmh",
     stream: "http://91.213.196.100:8000/300kmh-192"
   },
 
   {
-    name: "186mph",
+    name: "Pdj 186mph",
     stream: "http://91.213.196.100:8000/186mph-192"
   },
 
   {
-    name: "Deep",
+    name: "Pdj Deep",
     stream: "http://91.213.196.100:8000/deep-192"
   },
 
   {
-    name: "Too Deep",
+    name: "Pdj Too Deep",
     stream: "http://91.213.196.100:8000/toodeep-192"
   },
 
   {
-    name: "Mini",
+    name: "Pdj Mini",
     stream: "http://91.213.196.100:8000/mini-192"
   },
 
   {
-    name: "Pop",
+    name: "Pdj Pop",
     stream: "http://91.213.196.100:8000/pop-192"
   },
 
   {
-    name: "Strange",
+    name: "Pdj Strange",
     stream: "http://91.213.196.100:8000/strange-192"
   },
 
   {
-    name: "Brainfck",
+    name: "Pdj Brainfck",
     stream: "http://91.213.196.100:8000/brainfck-192"
   },
 
   {
-    name: "Pioneer DJ Radio",
-    stream: "http://91.213.196.100:8000/pioneerdj-192"
-  },
-
-  {
-    name: "Bobina",
+    name: "Pdj Bobina",
     stream: "http://91.213.196.100:8000/bobina-192"
   },
 
   {
-    name: "DJ GROOVE",
+    name: "Pdj DJ GROOVE",
     stream: "http://91.213.196.100:8000/groove-192"
   },
 
   {
-    name: "Mixadance FM",
+    name: "Pdj Mixadance FM",
     stream: "http://91.213.196.100:8000/mixadancefm-192"
   }
 ];
