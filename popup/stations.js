1b2d959835ac6294201a2ad4faa7c17bbe755d7c
tryasko <tryasko@gmail.com>
2018-02-19 16:02:24 +0200

    Update station list (Move station list as external file)

---
+++ popup/stations.js
@@ -0,0 +1,156 @@
+window.stationList = [
+  {
+    name: "KissFM UA",
+    stream: "http://online-kissfm.tavrmedia.ua/KissFM"
+  },
+
+  {
+    name: "Radio ROKS",
+    stream: "http://online-radioroks.tavrmedia.ua/RadioROKS"
+  },
+
+  {
+    name: "Radio Feel",
+    stream: "http://62.80.190.246:8000/Feel"
+  },
+
+  {
+    name: "Pirate Station",
+    stream: "http://air2.radiorecord.ru:805/ps_128"
+  },
+
+  {
+    name: "Radio Record",
+    stream: "http://air2.radiorecord.ru:805/rr_128"
+  },
+
+  {
+    name: "NRJ Ru",
+    stream: "http://ic4.101.ru:8000/v1_1"
+  },
+
+  {
+    name: "Maximum",
+    stream: "http://icecast.radiomaximum.cdnvideo.ru/maximum.mp3"
+  },
+
+  {
+    name: "GarageFM",
+    stream: "http://91.213.196.100:8000/garagefm-192"
+  },
+
+  {
+    name: "TOP 100",
+    stream: "http://91.213.196.100:8000/top100-192"
+  },
+
+  {
+    name: "Old-School",
+    stream: "http://91.213.196.100:8000/oldschool-192"
+  },
+
+  {
+    name: "Channel 5",
+    stream: "http://91.213.196.100:8000/channel5-192"
+  },
+
+  {
+    name: "Too Deep",
+    stream: "http://91.213.196.100:8000/toodeep-192"
+  },
+
+  {
+    name: "Klubb",
+    stream: "http://91.213.196.100:8000/klubb-192"
+  },
+
+  {
+    name: "Dubstep",
+    stream: "http://91.213.196.100:8000/dubstep-192"
+  },
+
+  {
+    name: "Nu",
+    stream: "http://91.213.196.100:8000/nu-192"
+  },
+
+  {
+    name: "Too Nu",
+    stream: "http://91.213.196.100:8000/toonu-192"
+  },
+
+  {
+    name: "Yo",
+    stream: "http://91.213.196.100:8000/yo-192"
+  },
+
+  {
+    name: "Full Moon",
+    stream: "http://91.213.196.100:8000/fullmoon-192"
+  },
+
+  {
+    name: "Vata",
+    stream: "http://91.213.196.100:8000/vata-192"
+  },
+
+  {
+    name: "300kmh",
+    stream: "http://91.213.196.100:8000/300kmh-192"
+  },
+
+  {
+    name: "186mph",
+    stream: "http://91.213.196.100:8000/186mph-192"
+  },
+
+  {
+    name: "Deep",
+    stream: "http://91.213.196.100:8000/deep-192"
+  },
+
+  {
+    name: "Too Deep",
+    stream: "http://91.213.196.100:8000/toodeep-192"
+  },
+
+  {
+    name: "Mini",
+    stream: "http://91.213.196.100:8000/mini-192"
+  },
+
+  {
+    name: "Pop",
+    stream: "http://91.213.196.100:8000/pop-192"
+  },
+
+  {
+    name: "Strange",
+    stream: "http://91.213.196.100:8000/strange-192"
+  },
+
+  {
+    name: "Brainfck",
+    stream: "http://91.213.196.100:8000/brainfck-192"
+  },
+
+  {
+    name: "Pioneer DJ Radio",
+    stream: "http://91.213.196.100:8000/pioneerdj-192"
+  },
+
+  {
+    name: "Bobina",
+    stream: "http://91.213.196.100:8000/bobina-192"
+  },
+
+  {
+    name: "DJ GROOVE",
+    stream: "http://91.213.196.100:8000/groove-192"
+  },
+
+  {
+    name: "Mixadance FM",
+    stream: "http://91.213.196.100:8000/mixadancefm-192"
+  }
+];
