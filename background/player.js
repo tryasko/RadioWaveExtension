9da18a59958e4c40e2d0a0b7ee3701f18b198f2c
tryasko <tryasko@gmail.com>
2018-02-22 16:54:03 +0200

    Some clearing

---
+++ background/player.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//
+// background player
+window.player = new class {
+  constructor() {
+    this.audio = new Audio();
+    this.storage = localStorage;
+  }
+
+  setPlay() {
+    this.setVolume();
+    this.audio.src = this.storage.stream;
+    this.audio.play();
+  }
+
+  setStop() {
+    this.audio.src = "";
+  }
+
+  setVolume() {
+    this.audio.volume = this.storage.volume / 100;
+  }
+}();
--- background/updater.js
+++ background/updater.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//
 // update localStorage, use new data structure
 (() => {
   const newState = { state: "stopped", volume: 30, stream: "http://online-kissfm.tavrmedia.ua/KissFM" };
@@ -8,32 +9,10 @@
   const state = {
     state: newState.state,
     volume: oldState.volume || newState.volume,
-    stream: oldState.url || newState.stream
+    stream: oldState.stream || newState.stream
   };
 
   localStorage.clear();
 
   Object.keys(state).forEach(key => localStorage.setItem(key, state[key]));
 })();
-
-// background player
-window.player = new class {
-  constructor() {
-    this.audio = new Audio();
-    this.storage = localStorage;
-  }
-
-  setPlay() {
-    this.setVolume();
-    this.audio.src = this.storage.stream;
-    this.audio.play();
-  }
-
-  setStop() {
-    this.audio.src = "";
-  }
-
-  setVolume() {
-    this.audio.volume = this.storage.volume / 100;
-  }
-}();
