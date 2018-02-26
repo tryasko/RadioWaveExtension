4928f4f2a24e521733491e07ed1898119f0defb5
tryasko <tryasko@gmail.com>
2018-02-26 21:20:56 +0200

    Add new stations

--- popup/stations.js
+++ popup/stations.js
@@ -1,22 +1,121 @@
 window.stationList = [
+  //UA
   {
-    name: "KissFM",
+    name: "KISS FM Ukraine",
     group: "UA",
-    stream: "http://online-kissfm.tavrmedia.ua/KissFM"
+    stream: "http://online-kissfm.tavrmedia.ua/KissFM",
+    url: "https://www.kissfm.ua/"
   },
 
   {
     name: "Radio ROKS",
     group: "UA",
-    stream: "http://online-radioroks.tavrmedia.ua/RadioROKS"
+    stream: "http://online-radioroks.tavrmedia.ua/RadioROKS",
+    url: "https://www.radioroks.ua/"
   },
 
   {
     name: "Radio Feel",
     group: "UA",
-    stream: "http://62.80.190.246:8000/Feel"
+    stream: "http://62.80.190.246:8000/Feel",
+    url: ""
   },
 
+  {
+    name: "Skovoroda Music",
+    group: "UA",
+    stream: "http://stream.radioskovoroda.com:8060/skovoroda_music",
+    url: "http://radioskovoroda.com/music"
+  },
+
+  {
+    name: "Galychyna FM",
+    group: "UA",
+    stream: "http://ua.uar.net:8000/galychyna",
+    url: "http://www.galychyna.fm/"
+  },
+
+  {
+    name: "GIKRadion",
+    group: "UA",
+    stream: "http://a5.radioheart.ru:8007/live192",
+    url: "https://gikradio.com/"
+  },
+
+  {
+    name: "NRJ Ukraine",
+    group: "UA",
+    stream: "http://cast.nrj.in.ua/nrj",
+    url: "http://nrj.ua/"
+  },
+
+  //RU
+  {
+    name: "GarageFM",
+    group: "RU",
+    stream: "http://91.213.196.100:8000/garagefm-192",
+    url: "http://garagefm.ru/"
+  },
+
+  {
+    name: "NRJ Russia",
+    group: "RU",
+    stream: "http://ic4.101.ru:8000/v1_1",
+    stream2: "http://ic7.101.ru:8000/",
+    url: "http://www.energyfm.ru/"
+  },
+
+  {
+    name: "Mixadance FM",
+    group: "RU",
+    stream: "http://91.213.196.100:8000/mixadancefm-192",
+    url: "https://www.mixadance.fm/"
+  },
+
+  //MJY
+  {
+    name: "RzK",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/radio-z-kryjivky"
+  },
+
+  {
+    name: "Greatest Songs Radio",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/radio-great"
+  },
+
+  {
+    name: "Mousse Radio",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/radio-mousse"
+  },
+
+  {
+    name: "Urban Space Radio",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/urban-space-radio"
+  },
+
+  {
+    name: "Radio 412",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/radio-412"
+  },
+
+  {
+    name: "Radio EgoЇсти",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/radio-egoisty"
+  },
+
+  {
+    name: "Kredens Cafe Radio",
+    group: "MJY",
+    stream: "http://mjoystream.westeurotele.com:8000/kredens-cafe-radio_mp3"
+  },
+
+  //RCD
   {
     name: "Pirate Station",
     group: "RCD",
@@ -29,147 +128,151 @@ window.stationList = [
     stream: "http://air2.radiorecord.ru:805/rr_128"
   },
 
-  {
-    name: "NRJ",
-    group: "RU",
-    stream: "http://ic4.101.ru:8000/v1_1"
-  },
-
+  //PDJ
   {
     name: "TOP 100",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/top100-192"
+    stream: "http://91.213.196.100:8000/top100-192",
+    url: "http://promodj.com/radio#top100"
   },
 
   {
     name: "Channel 5",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/channel5-192"
-  },
-
-  {
-    name: "GarageFM",
-    group: "PDJ",
-    stream: "http://91.213.196.100:8000/garagefm-192"
+    stream: "http://91.213.196.100:8000/channel5-192",
+    url: "http://promodj.com/radio#channel5"
   },
 
   {
     name: "Trap",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/trap-192"
+    stream: "http://91.213.196.100:8000/trap-192",
+    url: "http://promodj.com/radio#trap"
   },
 
   {
     name: "Klubb",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/klubb-192"
+    stream: "http://91.213.196.100:8000/klubb-192",
+    url: "http://promodj.com/radio#klubb"
   },
 
   {
     name: "Dubstep",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/dubstep-192"
+    stream: "http://91.213.196.100:8000/dubstep-192",
+    url: "http://promodj.com/radio#dubstep"
   },
 
   {
     name: "Nu",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/nu-192"
+    stream: "http://91.213.196.100:8000/nu-192",
+    url: "http://promodj.com/radio#nu"
   },
 
   {
     name: "Too Nu",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/toonu-192"
+    stream: "http://91.213.196.100:8000/toonu-192",
+    url: "http://promodj.com/radio#toonu"
   },
 
   {
     name: "Yo",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/yo-192"
+    stream: "http://91.213.196.100:8000/yo-192",
+    url: "http://promodj.com/radio#yo"
   },
 
   {
     name: "Full Moon",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/fullmoon-192"
+    stream: "http://91.213.196.100:8000/fullmoon-192",
+    url: "http://promodj.com/radio#fullmoon"
   },
 
   {
     name: "Vata",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/vata-192"
+    stream: "http://91.213.196.100:8000/vata-192",
+    url: "http://promodj.com/radio#vata"
   },
 
   {
     name: "300kmh",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/300kmh-192"
+    stream: "http://91.213.196.100:8000/300kmh-192",
+    url: "http://promodj.com/radio#300kmh"
   },
 
   {
     name: "186mph",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/186mph-192"
+    stream: "http://91.213.196.100:8000/186mph-192",
+    url: "http://promodj.com/radio#186mph"
   },
 
   {
     name: "Deep",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/deep-192"
+    stream: "http://91.213.196.100:8000/deep-192",
+    url: "http://promodj.com/radio#deep"
   },
 
   {
     name: "Too Deep",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/toodeep-192"
+    stream: "http://91.213.196.100:8000/toodeep-192",
+    url: "http://promodj.com/radio#toodeep"
   },
 
   {
     name: "Mini",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/mini-192"
+    stream: "http://91.213.196.100:8000/mini-192",
+    url: "http://promodj.com/radio#mini"
   },
 
   {
     name: "Pop",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/pop-192"
+    stream: "http://91.213.196.100:8000/pop-192",
+    url: "http://promodj.com/radio#pop"
   },
 
   {
     name: "Old-School",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/oldschool-192"
+    stream: "http://91.213.196.100:8000/oldschool-192",
+    url: "http://promodj.com/radio#oldschool"
   },
 
   {
     name: "Strange",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/strange-192"
+    stream: "http://91.213.196.100:8000/strange-192",
+    url: "http://promodj.com/radio#strange"
   },
 
   {
     name: "Brainfck",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/brainfck-192"
+    stream: "http://91.213.196.100:8000/brainfck-192",
+    url: "http://promodj.com/radio#brainfck"
   },
 
   {
     name: "Bobina",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/bobina-192"
+    stream: "http://91.213.196.100:8000/bobina-192",
+    url: "http://promodj.com/radio#bobina"
   },
 
   {
     name: "DJ GROOVE",
     group: "PDJ",
-    stream: "http://91.213.196.100:8000/groove-192"
-  },
-
-  {
-    name: "Mixadance FM",
-    group: "PDJ",
-    stream: "http://91.213.196.100:8000/mixadancefm-192"
+    stream: "http://91.213.196.100:8000/groove-192",
+    url: "http://promodj.com/radio#groove"
   }
 ];
