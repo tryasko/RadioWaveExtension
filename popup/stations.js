d40e41d2222dd82becd7c35db6507dfacfe8a1ae
tryasko <tryasko@gmail.com>
2018-03-09 19:34:36 +0200

    Added new station group GDR gaduradio.pl

--- popup/stations.js
+++ popup/stations.js
@@ -18,7 +18,7 @@ window.stationList = [
     name: "Radio Feel",
     group: "UA",
     stream: "http://62.80.190.246:8000/Feel",
-    url: ""
+    url: "http://radiowave.in.ua/"
   },
 
   {
@@ -49,6 +49,13 @@ window.stationList = [
     url: "http://nrj.ua/"
   },
 
+  {
+    name: "MFM STATION",
+    group: "UA",
+    stream: "http://radio.mfm.ua/online128",
+    url: "http://mfm.ua/"
+  },
+
   //RU
   {
     name: "GarageFM",
@@ -72,63 +79,72 @@ window.stationList = [
     url: "https://www.mixadance.fm/"
   },
 
-  //MJY
+  //MJY http://mjoy.ua/radios/
   {
     name: "RzK",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/radio-z-kryjivky"
+    stream: "http://mjoystream.westeurotele.com:8000/radio-z-kryjivky",
+    url: "http://mjoy.ua/radio/rzk/"
   },
 
   {
     name: "Greatest Songs Radio",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/radio-great"
+    stream: "http://mjoystream.westeurotele.com:8000/radio-great",
+    url: "http://mjoy.ua/radio/radio-great/"
   },
 
   {
     name: "Mousse Radio",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/radio-mousse"
+    stream: "http://mjoystream.westeurotele.com:8000/radio-mousse",
+    url: "http://mjoy.ua/radio/radio-mousse/"
   },
 
   {
     name: "Urban Space Radio",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/urban-space-radio"
+    stream: "http://mjoystream.westeurotele.com:8000/urban-space-radio",
+    url: "http://mjoy.ua/radio/urban-space-radio/"
   },
 
   {
     name: "Radio 412",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/radio-412"
+    stream: "http://mjoystream.westeurotele.com:8000/radio-412",
+    url: "http://mjoy.ua/radio/412/"
   },
 
   {
     name: "Radio EgoЇсти",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/radio-egoisty"
+    stream: "http://mjoystream.westeurotele.com:8000/radio-egoisty",
+    url: "http://mjoy.ua/radio/ego/"
   },
 
   {
     name: "Kredens Cafe Radio",
     group: "MJY",
-    stream: "http://mjoystream.westeurotele.com:8000/kredens-cafe-radio_mp3"
+    stream: "http://mjoystream.westeurotele.com:8000/kredens-cafe-radio_mp3",
+    url: "http://mjoy.ua/radio/kredens-cafe-radio/"
   },
 
-  //RCD
+  //RCD http://www.radiorecord.ru/
   {
     name: "Pirate Station",
     group: "RCD",
-    stream: "http://air2.radiorecord.ru:805/ps_128"
+    stream: "http://air2.radiorecord.ru:805/ps_128",
+    url: "http://www.radiorecord.ru/radio/stations/?st=ps"
   },
 
   {
     name: "Radio Record",
     group: "RCD",
-    stream: "http://air2.radiorecord.ru:805/rr_128"
+    stream: "http://air2.radiorecord.ru:805/rr_128",
+    url: "http://www.radiorecord.ru/radio/stations/?st=rr"
   },
 
-  //PDJ
+  //PDJ http://promodj.com/
   {
     name: "TOP 100",
     group: "PDJ",
@@ -274,5 +290,76 @@ window.stationList = [
     group: "PDJ",
     stream: "http://91.213.196.100:8000/groove-192",
     url: "http://promodj.com/radio#groove"
+  },
+
+  //GDR http://gaduradio.pl/
+  {
+    name: "Weekend Hits",
+    group: "GDR",
+    stream: "http://stream.open.fm/127?type=.aac",
+    url: "http://gaduradio.pl/play/127"
+  },
+
+  {
+    name: "Impreza",
+    group: "GDR",
+    stream: "http://stream.open.fm/2?type=.aac",
+    url: "http://gaduradio.pl/play/2"
+  },
+
+  {
+    name: "Disco Polo",
+    group: "GDR",
+    stream: "http://stream.open.fm/21?type=.aac",
+    url: "http://gaduradio.pl/play/21"
+  },
+
+  {
+    name: "#popularne",
+    group: "GDR",
+    stream: "http://stream.open.fm/10?type=.aac",
+    url: "http://gaduradio.pl/play/10"
+  },
+
+  {
+    name: "Tylko Polskie Przeboje",
+    group: "GDR",
+    stream: "http://stream.open.fm/111?type=.aac",
+    url: "http://gaduradio.pl/play/111"
+  },
+
+  {
+    name: "Hip-Hop PL",
+    group: "GDR",
+    stream: "http://stream.open.fm/24?type=.aac",
+    url: "http://gaduradio.pl/play/24"
+  },
+
+  {
+    name: "Drum'n'Bass",
+    group: "GDR",
+    stream: "http://stream.open.fm/41?type=.aac",
+    url: "http://gaduradio.pl/play/41"
+  },
+
+  {
+    name: "Relaks",
+    group: "GDR",
+    stream: "http://stream.open.fm/112?type=.aac",
+    url: "http://gaduradio.pl/play/112"
+  },
+
+  {
+    name: "Top Wszech Czasów - Świat",
+    group: "GDR",
+    stream: "http://stream.open.fm/151?type=.aac",
+    url: "http://gaduradio.pl/play/151"
+  },
+
+  {
+    name: "500 Rock Hits",
+    group: "GDR",
+    stream: "http://stream.open.fm/82?type=.aac",
+    url: "http://gaduradio.pl/play/82"
   }
 ];
