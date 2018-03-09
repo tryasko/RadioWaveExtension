446a501baafc3057543b2ce65cbf2194377d3fb3
tryasko <tryasko@gmail.com>
2018-03-09 22:24:46 +0200

    Added all station to RadioRecord group

--- popup/stations.js
+++ popup/stations.js
@@ -36,7 +36,7 @@ window.stationList = [
   },
 
   {
-    name: "GIKRadion",
+    name: "GIKRadio",
     group: "UA",
     stream: "http://a5.radioheart.ru:8007/live192",
     url: "https://gikradio.com/"
@@ -50,7 +50,7 @@ window.stationList = [
   },
 
   {
-    name: "MFM STATION",
+    name: "MFM Station",
     group: "UA",
     stream: "http://radio.mfm.ua/online128",
     url: "http://mfm.ua/"
@@ -131,17 +131,254 @@ window.stationList = [
 
   //RCD http://www.radiorecord.ru/
   {
+    name: "Radio Record",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/rr_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=rr"
+  },
+
+  {
+    name: "Record Megamix",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/mix_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=mix"
+  },
+
+  {
+    name: "Record Deep",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/deep_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=deep"
+  },
+
+  {
+    name: "Record EDM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/club_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=club"
+  },
+
+  {
+    name: "Future House",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/fut_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=fut"
+  },
+
+  {
+    name: "Trancemission Radio",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/tm_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=tm"
+  },
+
+  {
+    name: "Record Chill-Out",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/chil_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=chil"
+  },
+
+  {
+    name: "Minimal / Tech",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/mini_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=mini"
+  },
+
+  {
     name: "Pirate Station",
     group: "RCD",
-    stream: "http://air2.radiorecord.ru:805/ps_128",
+    stream: "http://air.radiorecord.ru:805/ps_320",
     url: "http://www.radiorecord.ru/radio/stations/?st=ps"
   },
 
   {
-    name: "Radio Record",
+    name: "Russian Mix",
     group: "RCD",
-    stream: "http://air2.radiorecord.ru:805/rr_128",
-    url: "http://www.radiorecord.ru/radio/stations/?st=rr"
+    stream: "http://air.radiorecord.ru:805/rus_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=rus"
+  },
+
+  {
+    name: "Vip House",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/vip_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=vip"
+  },
+
+  {
+    name: "Супердискотека 90-х",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/sd90_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=sd90"
+  },
+
+  {
+    name: "Record Breaks",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/brks_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=brks"
+  },
+
+  {
+    name: "Record Dubstep",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/dub_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=dub"
+  },
+
+  {
+    name: "Record Dancecore",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/dc_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=dc"
+  },
+
+  {
+    name: "Record Techno",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/techno_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=techno"
+  },
+
+  {
+    name: "Record Hardstyle",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/teo_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=teo"
+  },
+
+  {
+    name: "Record Trap",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/trap_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=trap"
+  },
+
+  {
+    name: "Old School",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/pump_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=pump"
+  },
+  {
+    name: "Record Rock",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/rock_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=rock"
+  },
+
+  {
+    name: "Медляк FM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/mdl_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=mdl"
+  },
+
+  {
+    name: "Гоп FM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/gop_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=gop"
+  },
+
+  {
+    name: "Black",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/yo_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=yo"
+  },
+
+  {
+    name: "Rave FM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/rave_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=rave"
+  },
+
+  {
+    name: "GOA/PSY",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/goa_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=goa"
+  },
+
+  {
+    name: "Tropical",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/trop_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=trop"
+  },
+
+  {
+    name: "Нафталин FM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/naft_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=naft"
+  },
+
+  {
+    name: "Gold",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/gold_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=gold"
+  },
+
+  {
+    name: "Record Techno",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/techno_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=techno"
+  },
+
+  {
+    name: "Remix",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/rmx_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=rmx"
+  },
+
+  {
+    name: "Hard Bass",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/hbass_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=hbass"
+  },
+
+  {
+    name: "Future Bass",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/fbass_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=fbass"
+  },
+
+  {
+    name: "Аншлаг FM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/ansh_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=ansh"
+  },
+
+  {
+    name: "Гастарбайтер FM",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/gast_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=gast"
+  },
+
+  {
+    name: "Innocence (Ibiza)",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/ibiza_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=ibiza"
+  },
+
+  {
+    name: "Маятник Фуко",
+    group: "RCD",
+    stream: "http://air.radiorecord.ru:805/mf_320",
+    url: "http://www.radiorecord.ru/radio/stations/?st=mf"
   },
 
   //PDJ http://promodj.com/
@@ -350,7 +587,7 @@ window.stationList = [
   },
 
   {
-    name: "Top Wszech Czasów - Świat",
+    name: "Top Wszech Czasów",
     group: "GDR",
     stream: "http://stream.open.fm/151?type=.aac",
     url: "http://gaduradio.pl/play/151"
