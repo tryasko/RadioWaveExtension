4513fd655f9230f781dc6d0a0dd5c879b8cd8639
tryasko <tryasko@gmail.com>
2018-04-05 17:30:24 +0300

    Fake analyst

--- source/background/index.js
+++ source/background/index.js
@@ -1,6 +1,21 @@
-import updater from "./scripts/updater";
-import stations from "./scripts/stations";
-import Player from "./scripts/player";
+import Analyst from "./scripts/analyst/";
+import Player from "./scripts/player/";
 
-updater(stations);
 window.player = new Player();
+window.analyst = new Analyst();
+
+window.chrome.runtime.onInstalled.addListener(({ reason, id, previousVersion }) => {
+  if (reason === "install") {
+    window.analyst.register();
+  }
+
+  if (reason === "update") {
+    window.analyst.state();
+  }
+});
+
+//
+// need refactor
+import updater from "./scripts/updater/";
+import stations from "./scripts/stations/";
+updater(stations);
--- source/options/index.jsx
+++ source/options/index.jsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import { h, render } from "preact";
 
-import Router from "./router";
+import Router from "./router/";
 
 render(<Router />, document.getElementById("root"));
