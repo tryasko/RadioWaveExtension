680706a076327539561da12c87dea3515f2baa9d
Ruslan Tryasko <681362+tryasko@users.noreply.github.com>
2024-08-20 18:38:14 +0200

    rename stream to station

--- shared/stations.js
+++ shared/stations.js
@@ -2,54 +2,54 @@ window.stationList = [
   {
     name: "MFM",
     group: "UA",
-    stream: "station=ua.mfm"
+    station: "ua.mfm"
   },
 
   {
     name: "Хіт FM",
     group: "TVR",
-    stream: "station=tvr.hit_fm"
+    station: "tvr.hit_fm"
   },
 
   {
     name: "Русское Радио UA",
     group: "TVR",
-    stream: "station=tvr.rusradio_ua"
+    station: "tvr.rusradio_ua"
   },
 
   {
     name: "Radio ROKS",
     group: "TVR",
-    stream: "station=tvr.radio_roks"
+    station: "tvr.radio_roks"
   },
 
   {
     name: "KISS FM UA",
     group: "TVR",
-    stream: "station=tvr.kiss_fm_ua"
+    station: "tvr.kiss_fm_ua"
   },
 
   {
     name: "Мелодія FM",
     group: "TVR",
-    stream: "station=tvr.melodia_fm"
+    station: "tvr.melodia_fm"
   },
 
   {
     name: "Радіо Relax",
     group: "TVR",
-    stream: "station=tvr.radio_relax"
+    station: "tvr.radio_relax"
   },
 
   {
     name: "Наше Радіо",
     group: "TVR",
-    stream: "station=tvr.nasheradio"
+    station: "tvr.nasheradio"
   },
 
   {
     name: "Radio Jazz",
     group: "TVR",
-    stream: "station=tvr.radiojazz"
+    station: "tvr.radiojazz"
   }
 ];
